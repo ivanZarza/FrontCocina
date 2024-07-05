6aa09259e1b9cb2abc0fcb0d63590ff8aa91a9e7
IvanZarza <ivanzarzaestevez@gmail.com>
2024-07-05 18:36:53 +0200

    wip

---
+++ Front ApiCocina/src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/ingredientes',
+      name: 'ingredientes',
+      component: () => import('../views/AboutView.vue')
+    },    
+    {
+      path: '/ingredientes/tipo',
+      name: 'tipos',
+      component: () => import('../views/AboutView.vue')
+    },    
+    {
+      path: '/ingredientes/panel',
+      name: 'panel',
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
