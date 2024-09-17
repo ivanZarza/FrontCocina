0924b3c5e87f0c53e781a3dade8c93e0fe03cec9
IvanZarza <ivanzarzaestevez@gmail.com>
2024-09-17 22:06:23 +0200

    wip

--- Front ApiCocina/src/views/usuarioLogeado/DatosLogeadoView.vue
+++ Front ApiCocina/src/views/usuarioLogeado/DatosLogeadoView.vue
@@ -6,34 +6,29 @@ const props = defineProps({
   usuarioId: Number
 })
 
-console.log(props.usuarioId);
-
 const usuario = ref({})
-const recetas = ref([])
+
+
+
 
 const service = servicioDatosUsuario
 service.obtenerDatosUsuario(props.usuarioId)
 usuario.value = service.datosUsuario
-recetas.value = service.recetasUsuario
-console.log(recetas.value);
+const recetas = service.recetasUsuario
 
-console.log(Array.isArray(recetas.value))
+console.log('Usuario', usuario.value)
+console.log('Recetas', recetas)
+
+
 </script>
 
 <template>
   <div>
     <div class="bienvenida">
       <h1>Hola {{ usuario.nombre }} {{ usuario.apellidos }}</h1>
-      <h3>Aqui podras recuperar tus recetas guardadas, tambien podras crear,modificar o eliminar tus propios
-        ingredientes
-      </h3>
-      <p>{{ recetas }}</p>
-      <div class="recetas" v-for="receta in recetas" :key="receta.id" >
-        <h2>{{ receta.id }}</h2>
-        <h2>{{ receta.usuarioId }}</h2>
-        <p>{{ receta.datosJSON }}</p>
-      </div>
+      <h3>Aquí podrás recuperar tus recetas guardadas, también podrás crear, modificar o eliminar tus propios
+        ingredientes</h3>
+        <p>esto que es?{{ recetas }}</p>
     </div>
   </div>
 </template>
-
